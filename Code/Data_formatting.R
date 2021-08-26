124b8623e740c1666760f21c7e04933d6513b1b8
adri-tab <adrien.tableau@ofb.gouv.fr>
2021-08-26 15:21:33 +0200

    Code and output added

---
+++ Code/Data_formatting.R
@@ -0,0 +1,456 @@
+Sys.setlocale("LC_ALL", "English")
+
+require(tidyverse)
+require(xlsx)
+require(readxl)
+require(lubridate)
+
+# UK data ---------------------------------------------------------------------------
+
+
+# Données de prélèvement 
+read_excel("../../../LIFE_Donnees/BDD_Oxyura/UK/Clean/cull.xlsx",
+           na = c("", "NA"), 
+           guess_max = 30000) %>% 
+  mutate(date = date(date)) -> UK_kill
+
+# données de comptages
+read_excel("../../../LIFE_Donnees/BDD_Oxyura/UK/Clean/count.xlsx", 
+           na = c("", "NA"), 
+           guess_max = 30000) %>%
+  mutate(date = date(date)) %>%
+  select(-age_sex, -...1) %>% 
+  arrange(date, site_code) -> UK_nb
+
+UK_nb %>% 
+  group_by(site_code, year = year(date), month = month(date)) %>% 
+  summarize(obs = max(obs)) %>% 
+  group_by(year, month) %>% 
+  summarize(obs = sum(obs)) %>% 
+  mutate(date = ymd(str_c(year, month, 15, sep = "-")),
+         season = case_when(month %in% c(12, 1, 2) ~ "winter",
+                            month %in% 3:5 ~ "spring",
+                            month %in% 6:8 ~ "summer",
+                            TRUE ~ "fall")) %>% 
+  ggplot(aes(x = date, y = obs, group = season, color = as_factor(season))) +
+  geom_point() +
+  geom_smooth(method = "gam") +
+  scale_x_date(date_labels = "%m-%Y")
+
+# -> détection plus importante en hiver et à l'automne qu'au printemps et à l'été
+# -> série hiver plus pertinente pour estimer la taille de pop
+
+UK_nb %>% 
+  mutate(mois = month(date)) %>%
+  group_by(source) %>% 
+  count(mois, name = "count") %>% 
+  ggplot(aes(x = mois, y = count, fill = source)) +
+  geom_col() + 
+  facet_wrap(~ source, ncol = 1) +
+  theme(legend.position = "none")
+
+UK_nb %>% 
+  filter(month(date) %in% c(12, 1)) %>% 
+  group_by(source) %>% 
+  count(date, name = "site_count") %>% 
+  ggplot(aes(x = date, y = site_count, color = source)) + 
+  geom_point() + 
+  facet_wrap(~ source, ncol = 1) +
+  theme(legend.position = "none")
+
+UK_nb %>% 
+  filter(!is.na(obs_type_fem) & !is.na(obs_type_mal)) %>%
+  mutate(year = if_else(month(date) == 1, year(date), year(date) + 1) %>% str_c("0101") %>% ymd()) %>% 
+  group_by(year, date) %>% 
+  summarize(across(starts_with("obs"), sum)) %>% 
+  ungroup() %>% 
+  mutate(obs = obs_type_mal + obs_type_fem,
+         prop = obs_type_mal / obs) %>% 
+  ggplot(aes(x = year, y = prop, size = obs)) +  
+  geom_smooth() +
+  geom_point() +
+  scale_size(trans = "log10")
+
+UK_nb %>% 
+  filter(!is.na(obs_type_fem) & !is.na(obs_type_mal)) %>%
+  mutate(year = if_else(month(date) == 1, year(date), year(date) + 1) %>% str_c("0101") %>% ymd()) %>% 
+  group_by(year, date) %>% 
+  summarize(across(starts_with("obs"), sum)) %>% 
+  ungroup() %>% 
+  select(-obs) -> UK_sex_app
+  
+# protocole mensuel pas les 0, donc pas sur que protocole soit carré...
+
+UK_nb %>% 
+  filter(month(date) %in% c(12, 1), obs != 0) %>%
+  mutate(date_av = ymd(str_c(year(date), month(date), 15, sep = "-"))) %>% 
+  group_by(site_code, date_av) %>% 
+  nest() %>% 
+  mutate(data = data %>% map( ~ .x %>% arrange(desc(obs)) %>% slice(1))) %>% 
+  unnest(data) -> UK_nb_2 
+  
+UK_nb_2 %>%
+  group_by(date_av) %>% 
+  add_count(name = "site") %>% 
+  mutate(count = sum(obs)) %>%
+  rowwise() %>% 
+  mutate(prop = obs / count) %>% 
+  group_by(date = date_av) %>% 
+  filter(prop == max(prop)) %>% 
+  ungroup() %>% 
+  select(date, count, site, source) %>% 
+  distinct() -> UK_nb_3
+
+UK_nb_3 %>% 
+  pivot_longer(cols = c(count, site), names_to = "var", values_to = "count") %>% 
+  ggplot(aes(x = date, y = count, shape = var, color = month(date, label = TRUE))) + 
+  geom_point() +
+  scale_y_log10()
+
+UK_nb_3 %>% 
+  mutate(year = if_else(month(date) == 12, date + days(17), date - days(14)),
+         source = str_c(month(date, label = TRUE, abbr = TRUE), 
+                        source %>% str_split(" ") %>% map_chr(last), sep = " - ")) %>% 
+  group_by(year) %>% 
+  filter(count == max(count)) %>% 
+  ungroup() %>% 
+  distinct() %>% 
+  mutate(end = c(.$date[-1], last(.$date) + years(1)) - days(1)) %>% 
+  select(year, start = date, end, count, site, source) -> UK_nb_4
+
+UK_nb_4 %>% 
+  ggplot(aes(x = year, y = count)) +
+  geom_line(linetype = "dashed") +
+  geom_point(aes(color = source))
+
+# prélèvements
+
+get_year <- function(x) {
+  UK_nb_4 %>% filter(start <= x, end >= x) %>% pull(year)}
+
+UK_kill %>% 
+  rowwise() %>% 
+  mutate(year = get_year(date)) %>% 
+  left_join(UK_nb_4 %>% select(start, end, year)) -> UK_kill_2
+  
+UK_kill_2 %>% 
+  group_by(year, age_sex) %>% 
+  summarize(across(shot, sum)) %>% 
+  ggplot(aes(x = year, y = shot, fill = age_sex)) +
+  geom_col()
+
+UK_kill_2 %>% 
+  mutate(repro = if_else(month(date) < 7, "before", "after"),
+         age_sex = age_sex %>% str_replace("no_", "no-"),
+         age = age_sex %>% str_extract(regex(".*(?=_)")),
+         sex = age_sex %>% str_extract(regex("(?<=_).*")),
+         age = if_else(age %in% c("ad", "ind"), age, "no_ad")) %>% 
+  group_by(year, start, end, repro, age, sex) %>% 
+  summarize(across(shot, sum)) %>%
+  ungroup() -> UK_kill_3
+
+crossing(UK_nb_4 %>% distinct(year, start, end), UK_kill_3 %>% distinct(repro, age, sex)) %>% 
+  left_join(UK_kill_3 %>% select(-c(start, end))) %>% 
+  mutate(shot = shot %>% replace_na(0)) -> UK_kill_4
+
+UK_kill_4 %>% 
+  filter(age != "ind", sex != "ind") %>% 
+  group_by(age) %>%
+  nest() %>% 
+  mutate(data = data %>% 
+           map( ~.x %>% 
+                  mutate(tot = sum(shot)) %>% 
+                  filter(sex == "mal") %>%
+                  summarize(mal = sum(shot),
+                            tot = unique(tot), 
+                            ratio = mal/tot))) %>%
+  unnest(data) %>% 
+  ungroup()
+# -> sexe ratio male : 60% pour les adultes, 54% pour les jeunes, pas d'évolution cohérente avant après printemps
+
+# age ratio 
+UK_kill %>% 
+  mutate(age = age_sex %>% str_extract(regex(".*(?=_)"))) %>% 
+  group_by(yday = yday(date),
+           year = year(date), 
+           age) %>% 
+  summarize(across(shot, sum)) %>% 
+  ungroup() -> UK_prop
+
+UK_prop %>% 
+  group_by(yday, age) %>% 
+  summarize(across(shot, sum)) %>% 
+  group_by(yday) %>% 
+  nest() %>% 
+  mutate(tot = data %>% map_dbl(~ .x %>% pull(shot) %>% sum())) %>% 
+  unnest(data) %>% 
+  ungroup() %>% 
+  mutate(prop = shot / tot) %>% 
+  ggplot(aes(x = yday,  y = prop, color = age)) +
+  geom_point() +
+  geom_smooth()
+# les anglais tuent toute l'année, on voit bien la période pritnanière ou tous sont adultes
+
+UK_prop %>% 
+  group_by(yday, age) %>% 
+  summarize(across(shot, sum)) %>% 
+  ggplot(aes(x = yday, y = shot, fill = age)) +
+  geom_col(position = "dodge")
+# gros prélèvement en hiver, donc on a la proportion du recrutement dans les hivernants
+
+UK_prop %>% 
+  group_by(yday) %>% 
+  nest() %>% 
+  mutate(tot = data %>% map_dbl(~ .x %>% filter(age!= "ind") %>% pull(shot) %>% sum())) %>% 
+  unnest(data) %>% 
+  ungroup() %>% 
+  mutate(prop = shot / tot) %>%
+  filter(age == "no_ad") %>% 
+  ggplot(aes(x = yday,  y = prop, color = tot)) +
+  geom_point() +
+  geom_smooth(aes(weight = tot)) +
+  facet_wrap(~ year) +
+  ylim(c(0, 1))
+
+# très variables d'une année à l'autre, mais il y a moyen d'estimer la prop de jeunes en prenant une fenetre large
+# Bourse de Fabricius visible 8 mois environ, source?
+# repro commence autour du 1er Juin donc jusqu'au 1er février, on détecte tous les jeunes. 
+# on peut faire une moyenne depuis le 1er décembre, lorsque les oiseaux commencent à hiverner
+
+UK_kill %>% 
+  filter(month(date) %in% c(12, 1)) %>% 
+  mutate(year = if_else(month(date) == 12, year(date) + 1, year(date)) %>% str_c("0101") %>% ymd) %>% 
+  filter(age_sex %>% str_detect("ad")) %>% 
+  group_by(year) %>% 
+  nest() %>% 
+  mutate(data = data %>% map(~ tibble(
+                                 ad = .x %>% filter(age_sex %>% str_starts("ad")) %>% pull(shot) %>% sum(),
+                                 no_ad = .x %>% filter(age_sex %>% str_detect("no_ad")) %>% pull(shot) %>% sum()))) %>% 
+  unnest(data) %>% 
+  ungroup() %>%
+  filter(ad + no_ad > 0) %>% 
+  arrange(year) -> UK_prop
+
+UK_nb_4 %>% 
+  select(year) %>% 
+  left_join(UK_prop) %>%
+  mutate(across(c(ad, no_ad), ~ .x %>% replace_na(0))) %>% 
+  group_by(year) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  rename(age = data) -> UK_prop_2
+
+UK_nb_4 %>% 
+  select(year) %>% 
+  full_join(UK_sex_app) %>% 
+  mutate(across(starts_with("obs"), ~ .x %>% replace_na(0))) %>% 
+  group_by(year) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  rename(sex_app = data) -> UK_sex_app_2
+
+UK_nb_4 %>% 
+  left_join(UK_prop_2) %>% 
+  left_join(UK_sex_app_2) -> UK_count
+
+# FR data ---------------------------------------------------------------------------
+
+read_excel("../../../LIFE_Donnees/BDD_Oxyura/FR/Observation_et_operation_2021-01-16.xlsx",
+           na = c("", "NA"),
+           col_types = c("date", rep("guess", 45)),
+           guess_max = 6000) %>%
+  mutate(Date = date(Date),
+         Dpt = as.character(Dpt)) %>%
+  filter(Date > ymd("1950-01-01")) -> FR_kill
+
+# données de comptages
+read_excel("../../../LIFE_Donnees/BDD_Oxyura/FR/Comptage_hiver_2021-01-16.xlsx", 
+           na = c("", "NA"), col_types = c("date", rep("numeric", 3))) %>% 
+  mutate(Date = date(Date)) %>% 
+  rename(date = Date, tot = Total, mal_nb = M, fem_nb = `F`) -> FR_nb
+
+# on récupère les données de comptages 
+FR_nb %>% 
+  mutate(year = if_else(month(date) > 6, year(date) + 1, year(date)) %>% str_c("0101") %>% ymd()) %>% 
+  arrange(year) %>% 
+  select(year, date, count = tot, obs_type_mal = mal_nb, obs_type_fem = fem_nb) -> FR_nb_2
+
+# on récupère les données de comptages dans la base générale
+FR_kill %>% 
+  filter(Lieu_nom == "LAC DE GRAND LIEU", 
+         month(Date) %in% c(10, 11, 12, 1, 2, 3),
+         !is.na(Nb_obs_tot)) %>%
+  mutate(year = if_else(month(Date) > 6, year(Date) + 1, year(Date)) %>% str_c("0101") %>% ymd()) %>% 
+  select(year, date = Date, count = Nb_obs_tot, obs_type_mal = Nb_obs_mal_ad, obs_type_fem = Nb_obs_fem_ad) %>% 
+  anti_join(FR_nb_2, by = c("date", "year")) %>%
+  bind_rows(FR_nb_2) %>% 
+  right_join(tibble(year = seq(min(.$year), max(.$year), by = "year"))) %>% 
+  arrange(year, date) %>% 
+  mutate(date = if_else(is.na(date), year, date),
+         across(where(is.numeric), ~ .x %>% replace_na(0))) -> FR_nb_3
+
+FR_nb_3 %>% 
+  group_by(year) %>% 
+  arrange(desc(count)) %>% 
+  rowid_to_column() %>% 
+  filter(rowid == min(rowid)) %>% 
+  ungroup() %>% 
+  arrange(year) %>% 
+  mutate(source = str_c(month(date, abbr = TRUE, label = TRUE), " - SNPN"),
+         site = 1,
+         start = year,
+         end = year + years(1), 
+         ad = 0, 
+         no_ad = 0) %>% 
+  group_by(across(-c(ad, no_ad))) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  rename(age = data) %>% 
+  left_join(FR_nb_3 %>% 
+              select(-count) %>% 
+              group_by(year) %>% 
+              nest() %>% 
+              ungroup() %>% 
+              rename(sex_app = data)) %>% 
+  select(year, start, end, count, site, source, age, sex_app) -> FR_count
+
+# données shot
+FR_kill %>% 
+  select(Date, starts_with("Nb_tue"), -Nb_tue_oeuf, Nb_obs_oeuf) %>% 
+  rename(Nb_tue_oeuf = Nb_obs_oeuf) %>% 
+  filter((!is.na(Nb_tue_tot)) | 
+           !(is.na(Nb_tue_mal_ad) & 
+               is.na(Nb_tue_fem_ad) & 
+               is.na(Nb_tue_juv) &
+               is.na(Nb_tue_pulli) & 
+               is.na(Nb_tue_ind))) %>% 
+  rowwise() %>% 
+  mutate_at(vars(starts_with("Nb")), replace_na, 0) %>% 
+  mutate(
+    check = sum(Nb_tue_fem_ad, Nb_tue_mal_ad, Nb_tue_juv, Nb_tue_pulli, Nb_tue_ind, Nb_tue_oeuf),
+    Nb_tue_tot = case_when(
+      is.na(Nb_tue_tot) ~ check,
+      Nb_tue_tot < check ~ check,
+      TRUE ~ Nb_tue_tot),
+    Nb_tue_ind = if_else(check < Nb_tue_tot, Nb_tue_ind + (Nb_tue_tot - check), Nb_tue_ind)
+  ) %>% 
+  filter(Nb_tue_tot > 0) %>% 
+  mutate(ad_fem = Nb_tue_fem_ad, 
+         ad_mal = Nb_tue_mal_ad, 
+         jeu_ind = Nb_tue_juv + Nb_tue_pulli + Nb_tue_oeuf, 
+         ind_ind = max(Nb_tue_ind, Nb_tue_tot - ad_fem - ad_mal - jeu_ind)) %>%
+  select(date = Date, !starts_with("Nb"), -check) %>% 
+  ungroup() %>% 
+  pivot_longer(cols = -date, names_to = "age_sex", values_to = "shot") %>% 
+  mutate(repro = if_else(month(date) < 7, "before", "after"),
+         year = date %>% as.character %>% str_trunc(4, "right", "") %>% str_c("01-01") %>% ymd(), 
+         age = age_sex %>% str_extract(regex(".*(?=_)")),
+         sex = age_sex %>% str_extract(regex("(?<=_).*")),
+         age = if_else(age == "jeu", "no_ad", age)) %>% 
+  select(year, repro, age, sex, shot) %>% 
+  group_by(across(-shot)) %>% 
+  summarize(across(shot, sum)) %>% 
+  ungroup() -> FR_kill_2
+
+# unique dataset
+crossing(year = seq(ymd(19600101), ymd(20210101), "year"), 
+         repro = c("before", "after"), 
+         age = c("ad", "no_ad", "ind"), 
+         sex = c("fem", "mal", "ind")) %>% 
+  filter(!(age == "ind" & sex != "ind")) -> base
+
+base %>% 
+  left_join(UK_kill_4) %>% 
+  mutate(shot = shot %>% replace_na(0), 
+         pop = "UK") %>% 
+  mutate(across(c(start, end), ~ if_else(is.na(.x), year, year + years(1)))) %>% 
+  select(year, start, end, pop, repro, age, sex, shot) -> UK_frag
+
+base %>%
+  left_join(FR_kill_2) %>% 
+  mutate(shot = shot %>% replace_na(0), 
+         pop = "FR",
+         start = year,
+         end = year + years(1)) %>% 
+  select(year, start, end, pop, repro, age, sex, shot) -> FR_frag
+
+bind_rows(UK_frag, FR_frag) -> frag
+
+frag %>% 
+  ggplot(aes(x = year, y = shot, fill = interaction(sex, age))) + 
+  geom_col() + 
+  facet_wrap(~ pop, ncol = 1, scales = "free_y")
+
+base %>% 
+  distinct(year) %>%
+  left_join(UK_count) %>% 
+  mutate(pop = "UK") -> UK_count 
+
+base %>% 
+distinct(year) %>% 
+  left_join(FR_count) %>% 
+  mutate(pop = "FR") -> FR_count
+
+bind_rows(UK_count, FR_count) %>% 
+  mutate(start = if_else(is.na(start), year, start),
+         end = if_else(is.na(end), year + years(1), end),
+         across(c(count, site), ~ .x %>% replace_na(0))) -> count
+
+count %>% 
+  ggplot(aes(x = year, y = count, color = pop)) + 
+  geom_line() +
+  geom_point() +
+  facet_wrap(~ pop, scales = "free_y", ncol = 1) +
+  theme(legend.position = "none")
+
+list(frag, count) %>% 
+  write_rds("./Data/Ruddy_duck_data.rds")
+
+# traitement des indéterminés
+FR_kill %>% 
+  select(Date, starts_with("Nb_tue"), -Nb_tue_oeuf, Nb_obs_oeuf) %>% 
+  rename(Nb_tue_oeuf = Nb_obs_oeuf) %>% 
+  filter((!is.na(Nb_tue_tot)) | 
+           !(is.na(Nb_tue_mal_ad) & 
+               is.na(Nb_tue_fem_ad) & 
+               is.na(Nb_tue_juv) &
+               is.na(Nb_tue_pulli) & 
+               is.na(Nb_tue_ind))) %>% 
+  rowwise() %>% 
+  mutate_at(vars(starts_with("Nb")), replace_na, 0) %>% 
+  mutate(
+    Nb_tue_tot = max(c(Nb_tue_tot, sum(Nb_tue_fem_ad, Nb_tue_mal_ad, Nb_tue_juv, Nb_tue_pulli, Nb_tue_ind, Nb_tue_oeuf))),
+    Nb_tue_ind = Nb_tue_tot - sum(Nb_tue_fem_ad, Nb_tue_mal_ad, Nb_tue_juv, Nb_tue_pulli, Nb_tue_oeuf)
+  ) %>% 
+  filter(Nb_tue_tot > 0) %>% 
+  mutate(ad = Nb_tue_fem_ad + Nb_tue_mal_ad, 
+         jeu = Nb_tue_juv + Nb_tue_pulli + Nb_tue_oeuf, 
+         ind = Nb_tue_ind) %>%
+  select(date = Date, !starts_with("Nb")) %>% 
+  ungroup() %>% 
+  pivot_longer(cols = -date, names_to = "age", values_to = "shot") -> ind
+
+ind %>% 
+  group_by(year = year(date), age) %>% 
+  summarise(across(shot, sum)) %>% 
+  ggplot(aes(x = year, y = shot, fill = age)) +
+  geom_col(position = "dodge") +
+  scale_y_sqrt()
+
+ind %>% 
+  group_by(yday = yday(date), age) %>% 
+  summarise(across(shot, sum)) %>% 
+  ggplot(aes(x = yday, y = shot, color = age)) +
+  geom_smooth()
+
+ind %>% 
+  group_by(yday = yday(date), age) %>% 
+  summarise(across(shot, sum)) %>% 
+  nest() %>% 
+  mutate(data = data %>% map(~ .x %>% mutate(shot = shot / sum(shot)))) %>% 
+  unnest(data) %>% 
+  ggplot(aes(x = yday, y = shot, color = age)) +
+  geom_smooth() +
+  geom_point()
+
+
